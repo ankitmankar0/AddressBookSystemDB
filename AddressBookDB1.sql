c88c414efa2c1db1713b10ee350c66c02bc26b34
ankitmankar0 <ankitmankar012@gmail.com>
2022-04-09 14:03:01 +0530

    Deleted Row Data

--- AddressBookDB1.sql
+++ AddressBookDB1.sql
@@ -26,3 +26,16 @@ select * from AddressBook;
 insert into AddressBook values('Ankit','Mankar','Patrahi Nagpur','Nagpur','Maharashtra','440023','8188888898','ankit@gmail.com');
 
 insert into AddressBook values('Rahul','Nayr','Nagpur','Nagpur','Maharashtra','442203','12345678910','opre@gmail.com');
+
+--UC4:- Ability to edit existing contact person using their name.
+update AddressBook set zip=444444 where firstName='Ankit';
+select * from AddressBook;
+
+update AddressBook set email='ankitmankar@gmail.com' where firstName='Ankit';
+
+update AddressBook set lastName='Singh' where firstName='Rahul';
+select * from AddressBook;
+
+--UC5:- Ability to delete a person using person's name.
+Delete AddressBook where firstName='Rahul';
+select * from AddressBook;
